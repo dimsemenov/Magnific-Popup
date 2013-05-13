dbe39a908e7b95c753e39e1aece847e846775be5
Dmitry Semenov <diiiimaaaa@gmail.com>
2013-05-13 18:38:32 +0300

    Optimized inline module

--- src/js/inline.js
+++ src/js/inline.js
@@ -1,10 +1,18 @@
 
 var INLINE_NS = 'inline',
-	_hasPlaceholder;
+	_hiddenClass,
+	_inlinePlaceholder, 
+	_lastInlineElement,
+	_putInlineElementsBack = function() {
+		if(_lastInlineElement) {
+			_inlinePlaceholder.after( _lastInlineElement.addClass(_hiddenClass) ).detach();
+			_lastInlineElement = null;
+		}
+	};
 
 $.magnificPopup.registerModule(INLINE_NS, {
 	options: {
-		hiddenClass: NS+'-hide',
+		hiddenClass: 'hide', // will be appended with `mfp-` prefix
 		markup: '',
 		tNotFound: 'Content not found'
 	},
@@ -12,60 +20,46 @@ $.magnificPopup.registerModule(INLINE_NS, {
 
 		initInline: function() {
 			mfp.types.push(INLINE_NS);
-			_hasPlaceholder = false;
 
 			_mfpOn(CLOSE_EVENT+'.'+INLINE_NS, function() {
-				var item = mfp.currItem;
-				if(item.type === INLINE_NS) {
-					if(_hasPlaceholder) {
-						for(var i = 0; i < mfp.items.length; i++) {
-							item = mfp.items[i];
-							if(item && item.inlinePlaceholder){
-								item.inlinePlaceholder.after( item.inlineElement.addClass(mfp.st.inline.hiddenClass) ).detach();
-							}
-						}
-					}
-					item.inlinePlaceholder = item.inlineElement = null;
-				}
+				_putInlineElementsBack();
 			});
 		},
 
 		getInline: function(item, template) {
-			mfp.updateStatus('ready');
+
+			_putInlineElementsBack();
 
 			if(item.src) {
-				var inlineSt = mfp.st.inline;
-				// items.src can be String-CSS-selector or jQuery element
-				if(typeof item.src !== 'string') {
-					item.isElement = true;
-				}
+				var inlineSt = mfp.st.inline,
+					el = $(item.src);
 
-				if(!item.isElement && !item.inlinePlaceholder) {
-					item.inlinePlaceholder = _getEl(inlineSt.hiddenClass);
-				}
-				
-				if(item.isElement) {
-					item.inlineElement = item.src;
-				} else if(!item.inlineElement) {
-					item.inlineElement = $(item.src);
-					if(!item.inlineElement.length) {
-						mfp.updateStatus('error', inlineSt.tNotFound);
-						item.inlineElement = $('<div>');
+				if(el.length) {
+
+					// If target element has parent - we replace it with placeholder and put it back after popup is closed
+					if(el[0].parentNode !== null) {
+						if(!_inlinePlaceholder) {
+							_hiddenClass = inlineSt.hiddenClass;
+							_inlinePlaceholder = _getEl(_hiddenClass);
+							_hiddenClass = 'mfp-'+_hiddenClass;
+						}
+						// replace target inline element with placeholder
+						_lastInlineElement = el.after(_inlinePlaceholder).detach().removeClass(_hiddenClass);
 					}
+
+					mfp.updateStatus('ready');
+				} else {
+					mfp.updateStatus('error', inlineSt.tNotFound);
+					el = $('<div>');
 				}
 
-				if(item.inlinePlaceholder) {
-					_hasPlaceholder = true;
-				}
-
-				
-				
-				item.inlineElement.after(item.inlinePlaceholder).detach().removeClass(inlineSt.hiddenClass);
-				return item.inlineElement;
-			} else {
-				mfp._parseMarkup(template, {}, item);
-				return template;
+				item.inlineElement = el;
+				return el;
 			}
+
+			mfp.updateStatus('ready');
+			mfp._parseMarkup(template, {}, item);
+			return template;
 		}
 	}
 });
