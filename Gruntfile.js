5bf56763cda6c5d910aa73ed86f5ee55f8d0ad39
Nick Schonning <nschonni@gmail.com>
2013-10-01 01:45:43 -0400

    Change contrib-sass to grunt-sass
    Removes Ruby dependency for compiling SCSS

--- Gruntfile.js
+++ Gruntfile.js
@@ -194,7 +194,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-sass');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-jekyll');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
