c6d13618e49b9d4c36891e142561212ac9a7f730
Thomas Walter <thomas.walter@brandnamic.com>
2014-05-23 09:43:35 +0200

    Fix recognition of jQuery for the _isJQ variable

--- src/js/core.js
+++ src/js/core.js
@@ -27,7 +27,7 @@ var CLOSE_EVENT = 'Close',
  */
 var mfp, // As we have only one instance of MagnificPopup object, we define it locally to not to use 'this'
 	MagnificPopup = function(){},
-	_isJQ = !!($),
+	_isJQ = (typeof jQuery !== 'undefined'),
 	_prevStatus,
 	_window = $(window),
 	_body,
