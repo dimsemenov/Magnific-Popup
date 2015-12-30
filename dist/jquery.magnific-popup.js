351cee14e6b90f2ef1a73c0b0678839482d1f959
Dmitry Semenov <diiiimaaaa@gmail.com>
2015-12-30 12:17:29 +0200

    1.0.1 version bump

--- dist/jquery.magnific-popup.js
+++ dist/jquery.magnific-popup.js
@@ -1,4 +1,4 @@
-/*! Magnific Popup - v1.0.0 - 2015-12-16
+/*! Magnific Popup - v1.0.1 - 2015-12-30
 * http://dimsemenov.com/plugins/magnific-popup/
 * Copyright (c) 2015 Dmitry Semenov; */
 ;(function (factory) { 
