3d93a49712674788800c1e147d8c26f8edc387c0
Hagen von Eitzen <eitzen@redeker.de>
2023-06-15 23:00:55 +0200

    replace deprecated (as per jQuery 3.x) methods

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -116,11 +116,11 @@ $.magnificPopup.registerModule('gallery', {
 						mfp.arrowPrev = arrowLeft;
 					}
 
-					arrowLeft.click(function() {
+					arrowLeft.on('click', function() {
 						if (gSt.langDir === 'rtl') mfp.next();
 						else mfp.prev();
 					});
-					arrowRight.click(function() {
+					arrowRight.on('click', function() {
 						if (gSt.langDir === 'rtl') mfp.prev();
 						else mfp.next();
 					});
